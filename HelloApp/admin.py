c02bbaa6a084f0f7631c9caad1589d9068de1262
unknown <aleksandr.lavrik@lavrik-a.metinvest.ua>
2015-03-05 10:46:55 +0200

    first commit

---
+++ HelloApp/admin.py
@@ -0,0 +1,20 @@
+# Register your models here.
+from django.contrib import admin
+
+from HelloApp.models import Question, Choice
+
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ('Ololoshenki-lolo', {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date']}),
+        ]
+    inlines = [ChoiceInline]
+
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice)
